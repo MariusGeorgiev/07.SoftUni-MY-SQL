cd473605c07547bb7d50a2fce830f92bd3fb1989
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-09-30 05:15:22 +0300

    Data Aggregation lab

---
+++ 04. Data Aggregation/01.Lab/Data Aggregation lab.sql
@@ -0,0 +1,29 @@
+-- 01
+SELECT department_id, COUNT(id) AS 'Number pf employees'
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+-- 02
+SELECT department_id, ROUND(AVG(salary), 2)
+ FROM employees
+ GROUP BY department_id;
+ 
+-- 03
+SELECT department_id, MIN(salary) AS 'Min Salary'
+FROM employees
+GROUP BY department_id
+HAVING `Min Salary` > 800;
+
+-- 04
+SELECT COUNT(*)
+FROM products
+WHERE category_id = 2 AND price > 8;
+
+-- 05
+SELECT category_id, 
+ROUND(AVG(price), 2) AS 'Average Price',
+MIN(price) AS 'Cheapest Product',
+MAX(price) AS 'Most Expensive Product'
+FROM products
+GROUP BY category_id;
